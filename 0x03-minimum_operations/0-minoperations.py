cc6a59deae1ec9f751598aaad817310df00dcc53
doubix <adib.grouz@gmail.com>
2020-08-08 14:43:26 +0100

    [add] mini oprs nbr

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""calculates the fewest number of operations needed to result in exactly n H characters in the file."""
+
+def minOperations(n):
+    """Calculate fewest nbr of opertions needed to result in n H chars
+    Args:
+        n (int): number of Chars
+    Returns:
+        (int): fewest nbr of opertions
+    """
+    if n <= 1:
+        return 0
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return minOperations(n // i) + i
