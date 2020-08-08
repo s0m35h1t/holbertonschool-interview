df2c935082c4caa259b79bd3b3e8a6832ec51daa
doubix <adib.grouz@gmail.com>
2020-08-08 14:45:06 +0100

    [fix] pep8 style

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""calculates the fewest number of operations needed to result in exactly n H characters in the file."""
+"""calculates the fewest number of operations needed
+to result in exactly n H characters in the file."""
+
 
 def minOperations(n):
     """Calculate fewest nbr of opertions needed to result in n H chars
