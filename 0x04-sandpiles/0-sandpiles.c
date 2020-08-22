45d1fdc5fbb160b52ea7063e9a88efe065f9f333
root <adib.grouz@gmail.com>
2020-08-22 03:51:22 +0100

    [fix] betty style

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -34,8 +34,7 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++)
 			grid1[i][j] += grid2[i][j];
-	do
-	{
+	do {
 		for (i = 0; i < 3; i++)
 			for (j = 0; j < 3; j++)
 				tmp[i][j] = grid1[i][j];
