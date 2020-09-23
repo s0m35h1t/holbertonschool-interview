5ae18dc5cf9d19588f44f3f238fbc0cd3efc2b4c
root <adib.grouz@gmail.com>
2020-09-23 21:18:02 +0100

    [add] utf-8 check fct

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -27,21 +27,17 @@ def validUTF8(data):
     #             b -= 1
     # return b == 0
     n_bytes = 0
-
     mask1 = 1 << 7
     mask2 = 1 << 6
 
     for num in data:
         mask = 1 << 7
-
         if n_bytes == 0:
             while(mask & num):
                 n_bytes += 1
                 mask = mask >> 1
-
             if(n_bytes == 0):
                 continue
-
             if n_bytes == 1 or n_bytes > 4:
                 return False
         else:
