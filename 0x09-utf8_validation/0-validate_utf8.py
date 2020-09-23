ff62e8a692d5a8da86c174743e00da261943ab60
root <adib.grouz@gmail.com>
2020-09-23 21:07:11 +0100

    [add] utf-8 check fct

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -11,14 +11,18 @@ def validUTF8(data):
         True if data is a valid UTF-8 encoding,
         else return False
     """
-    b = 0 
+    b = 0
     for x in data:
         x = bin(x)[2:].zfill(8)
         if b:
-            if x.startswith("10"): b -= 1
-            else: return False 
+            if x.startswith("10"):
+                b -= 1
+            else:
+                return False
         else:
             b = x.find("0")
-            if b == -1 or b == 1 or b > 4: return False 
-            if b: b -= 1
+            if b == -1 or b == 1 or b > 4:
+                return False
+            if b:
+                b -= 1
     return b == 0
