8dc15fa04261f24ca9a20871492de5a52be84569
root <adib.grouz@gmail.com>
2020-10-07 14:38:30 +0100

    [add] menger sponge

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,40 @@
+#include "menger.h"
+
+/**
+ * pchar - determines char to print '#' or ' '
+ * @row: row idx
+ * @col: column idx
+ *
+ * Return: '#' or ' '
+ */
+char pchar(size_t r, size_t c)
+{
+	while (r && c)
+	{
+		if (r % 3 == 1 && c % 3 == 1)
+			return (' ');
+		r /= 3, c /= 3;
+	}
+	return ('#');
+}
+
+/**
+ * menger - draws a 2D Menger sponge
+ * @level: level of Menger sponge to draw
+ *
+ * Return: None
+ */
+void menger(int level)
+{
+	size_t size, r, c;
+
+	if (level < 0)
+		return;
+	size = pow(3, level);
+	for (r = 0; r < size; ++r)
+	{
+		for (c = 0; c < size; ++c)
+			putchar(pchar(r, c));
+		putchar('\n');
+	}
+}
