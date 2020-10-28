7a08043275d878f371a9f0128911ceb72480da5c
root <root@DESKTOP-3L7UPTA.localdomain>
2020-10-28 20:11:28 +0100

    [Add] n queens

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def solve(row, col):
+    svr = [[]]
+    for q in range(row):
+        svr = place_queen(q, col, svr)
+    return svr
+
+
+def place_queen(q, col, prev_solver):
+    solver_queen = []
+    for arr in prev_solver:
+        for x in range(col):
+            if is_safe(q, x, arr):
+                solver_queen.append(arr + [x])
+    return solver_queen
+
+
+def is_safe(q, x, arr):
+    if x in arr:
+        return (False)
+    else:
+        return all(abs(arr[col] - x) != q - col
+                   for col in range(q))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit():
+        queen = int(sys.argv[1])
+    else:
+        print("N must be a number")
+        sys.exit(1)
+    if the_queen < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solver = solve(queen, queen)
+    for arr in solver:
+        cl = []
+        for q, x in enumerate(arr):
+            cl.append([q, x])
+        print(cl)
