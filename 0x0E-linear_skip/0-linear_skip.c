d9db879bb14a43a2a1669894f334b732e95af007
root <root@DESKTOP-3L7UPTA.localdomain>
2020-11-11 22:37:22 +0100

    [Fix] betty style

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -19,37 +19,31 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 		if (curr->express == NULL)
 		{
 			node = curr->express;
-			tmp = list;
-			while (tmp)
+			for (tmp = list; tmp; tmp = tmp->next)
 			{
 				if (tmp->next == NULL)
 				{
 					printf("Value found between indexes [%lu] and [%lu]\n",
-						   curr->index,
-						   tmp->index);
+						   curr->index, tmp->index);
 					break;
 				}
-				tmp = tmp->next;
 			}
 			break;
 		}
 		if (curr->express->n >= value)
 		{
 			node = curr->express;
-			printf("Value checked at index [%lu] = [%d]\n",
-				   node->index, node->n);
+			printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
 			printf("Value found between indexes [%lu] and [%lu]\n",
 				   curr->index, node->index);
 			break;
 		}
 		curr = curr->express;
-		printf("Value checked at index [%lu] = [%d]\n",
-			   curr->index, curr->n);
+		printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
 	}
 	while (curr)
 	{
-		printf("Value checked at index [%lu] = [%d]\n",
-			   curr->index, curr->n);
+		printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
 		if (curr->n == value)
 			return (curr);
 		curr = curr->next;
