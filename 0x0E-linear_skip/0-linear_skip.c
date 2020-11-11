195fb94a1ebba6052dcf6aacf1420915a6dc0460
root <root@DESKTOP-3L7UPTA.localdomain>
2020-11-11 19:20:02 +0100

    [Add] linear skip

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,62 @@
+#include "search.h"
+
+/**
+ * linear_skip - search for value in skip list of integers
+ * @list: pointer to head of skip list to search
+ * @value: value to search for
+ *
+ * Return: pointer to first node where value is located, otherwise NULL
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *curr, *node, *tmp;
+
+	if (list == 0)
+		return (NULL);
+	curr = list;
+
+	while (curr)
+	{
+		if (curr->express == NULL)
+		{
+			node = curr->express;
+			tmp = list;
+			while (tmp)
+			{
+
+				if (tmp->next == NULL)
+				{
+					printf("Value found between indexes [%lu] and [%lu]\n",
+						   curr->index,
+						   tmp->index);
+					break;
+				}
+				tmp = tmp->next;
+			}
+			break;
+		}
+
+		if (curr->express->n >= value)
+		{
+			node = curr->express;
+			printf("Value checked at index [%lu] = [%d]\n",
+				   node->index, node->n);
+			printf("Value found between indexes [%lu] and [%lu]\n",
+				   curr->index, node->index);
+			break;
+		}
+
+		curr = curr->express;
+		printf("Value checked at index [%lu] = [%d]\n",
+			   curr->index, curr->n);
+	}
+	while (curr)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+			   curr->index, curr->n);
+		if (curr->n == value)
+			return (curr);
+		curr = curr->next;
+	}
+	return (NULL);
+}
